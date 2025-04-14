4572683b4207d07d71e2f2b3ffdfbdac3c635f26
HamadSaeed22 <89844320+HamadSaeed22@users.noreply.github.com>
2025-04-14 11:39:09 +0400

    Arduino-wifi-version1-1.ino
    fixed some minor errors

--- Arduino-wifi-version1/Arduino-wifi-version1.ino
+++ Arduino-wifi-version1/Arduino-wifi-version1.ino
@@ -1,4 +1,4 @@
-#include <WiFiS3.h>
+#include <WiFi.h>
 #include <HTTPClient.h>
 #include <DHT.h>
 #include <OneWire.h>
@@ -6,37 +6,38 @@
 #include <Servo.h>
 
 // WiFi credentials
-const char* ssid = "Saeed-ALAli-3";
-const char* password = "Ses325_S";
+const char* ssid = "RIT-STUDENT";
+const char* password = "RitStd@123";
 
-// Google Apps Script Web App URL
-String webAppUrl = "https://script.google.com/macros/s/AKfycbzP1z94-cIR6UZZneeSpoHRNiNP-jrk_BQ3T_Ltj7bLQ3mBtOnpOzggdidJL2uC92GxGQ/exec";
+// Google Apps Script URL
+String webAppUrl = "https://script.google.com/macros/s/AKfycbzjMCcP0DYF9fO4DYgvBbGblm5h7LMdtssqe9sW9AjER0048rQUZBhDfrGFvwz3A5Fl8Q/exec";
 
-// DHT Pins & Type
-#define DHTTYPE DHT22
+// DHT sensor pins
 #define AMBIENT_DHTPIN 4
 #define DHTPIN2 5
 #define DHTPIN3 6
+
+// DS18B20 pin
+#define ONE_WIRE_BUS 10
+
+// Relay pins
+#define RELAY_PUMP 11
+#define RELAY_FAN1 12
+#define RELAY_FAN2 13
+
+// Servo pin
+#define SERVO_PIN 7
+
+#define DHTTYPE DHT22
 
 DHT ambientDHT(AMBIENT_DHTPIN, DHTTYPE);
 DHT dht2(DHTPIN2, DHTTYPE);
 DHT dht3(DHTPIN3, DHTTYPE);
 
-// DS18B20
-#define ONE_WIRE_BUS 10
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature ds18b20(&oneWire);
-
-// Relays
-#define RELAY_PUMP 11
-#define RELAY_FAN1 12
-#define RELAY_FAN2 13
-
-// Servo
-#define SERVO_PIN 7
 Servo flapServo;
 
-// Readings buffer
 const int numReadings = 5;
 float dhtReadings[2][numReadings] = {0};
 float dsReadings[6][numReadings] = {0};
@@ -45,31 +46,31 @@ int dhtIndex = 0, dsIndex = 0;
 void setup() {
   Serial.begin(115200);
 
-  // Sensor init
+  // Connect to WiFi
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("\nWiFi connected!");
+
+  // Sensor setup
   ambientDHT.begin();
   dht2.begin();
   dht3.begin();
   ds18b20.begin();
 
-  // Relay output
   pinMode(RELAY_PUMP, OUTPUT);
   pinMode(RELAY_FAN1, OUTPUT);
   pinMode(RELAY_FAN2, OUTPUT);
+
   digitalWrite(RELAY_PUMP, HIGH);
   digitalWrite(RELAY_FAN1, HIGH);
   digitalWrite(RELAY_FAN2, HIGH);
 
-  // Servo
   flapServo.attach(SERVO_PIN);
-  flapServo.write(180); // Closed
-
-  // Connect WiFi
-  Serial.print("Connecting to WiFi...");
-  while (WiFi.begin(ssid, password) != WL_CONNECTED) {
-    delay(1000);
-    Serial.print(".");
-  }
-  Serial.println(" Connected!");
+  flapServo.write(180); // Closed position
 }
 
 void loop() {
@@ -93,23 +94,15 @@ void loop() {
   dhtIndex = (dhtIndex + 1) % numReadings;
   dsIndex = (dsIndex + 1) % numReadings;
 
-  float avgDHT[2] = {0};
-  float avgDS[6] = {0};
-  int countDHT[2] = {0};
-  int countDS[6] = {0};
+  float avgDHT[2] = {0}, avgDS[6] = {0};
+  int countDHT[2] = {0}, countDS[6] = {0};
 
   for (int i = 0; i < numReadings; i++) {
     for (int j = 0; j < 2; j++) {
-      if (!isnan(dhtReadings[j][i])) {
-        avgDHT[j] += dhtReadings[j][i];
-        countDHT[j]++;
-      }
+      if (!isnan(dhtReadings[j][i])) { avgDHT[j] += dhtReadings[j][i]; countDHT[j]++; }
     }
     for (int j = 0; j < 6; j++) {
-      if (!isnan(dsReadings[j][i])) {
-        avgDS[j] += dsReadings[j][i];
-        countDS[j]++;
-      }
+      if (!isnan(dsReadings[j][i])) { avgDS[j] += dsReadings[j][i]; countDS[j]++; }
     }
   }
 
@@ -122,29 +115,22 @@ void loop() {
 
   bool enoughData = (dhtIndex == 0);
   if (enoughData) {
-    float allTemps[8];
     float totalAvg = 0;
     int validCount = 0;
+    float allTemps[8];
 
-    for (int i = 0; i < 2; i++) {
-      allTemps[i] = avgDHT[i];
-      if (!isnan(avgDHT[i])) {
-        totalAvg += avgDHT[i];
-        validCount++;
-      }
+    for (int j = 0; j < 2; j++) {
+      allTemps[j] = avgDHT[j];
+      if (!isnan(avgDHT[j])) { totalAvg += avgDHT[j]; validCount++; }
     }
-
-    for (int i = 0; i < 6; i++) {
-      allTemps[i + 2] = avgDS[i];
-      if (!isnan(avgDS[i])) {
-        totalAvg += avgDS[i];
-        validCount++;
-      }
+    for (int j = 0; j < 6; j++) {
+      allTemps[j + 2] = avgDS[j];
+      if (!isnan(avgDS[j])) { totalAvg += avgDS[j]; validCount++; }
     }
 
     float avgTemp = validCount > 0 ? totalAvg / validCount : NAN;
 
-    // Control relays
+    // Actuators
     digitalWrite(RELAY_FAN1, avgTemp >= 36 ? LOW : HIGH);
     digitalWrite(RELAY_FAN2, avgTemp >= 37.5 ? LOW : HIGH);
     digitalWrite(RELAY_PUMP, avgTemp >= 39 ? LOW : HIGH);
@@ -153,7 +139,6 @@ void loop() {
     String fan1State = digitalRead(RELAY_FAN1) == LOW ? "ON" : "OFF";
     String fan2State = digitalRead(RELAY_FAN2) == LOW ? "ON" : "OFF";
 
-    // Servo logic
     float servoPosition = 180;
     String servoStatus = "Closed";
     if (avgTemp >= 36) { servoPosition = 0; servoStatus = "100% Open"; }
@@ -162,36 +147,40 @@ void loop() {
     else if (avgTemp >= 33) { servoPosition = 135; servoStatus = "25% Open"; }
     flapServo.write(servoPosition);
 
-    // Build CSV string
-    String csvData = String(tempAmbient) + ",";
+    // Prepare CSV data
+    String data = String(tempAmbient) + ",";
     for (int i = 0; i < 8; i++) {
-      csvData += String(allTemps[i]) + ",";
+      data += String(allTemps[i]) + ",";
     }
-    csvData += String(avgTemp) + ",";
-    csvData += pumpState + "," + fan1State + "," + fan2State + "," + servoStatus;
+    data += String(avgTemp) + ",";
+    data += pumpState + "," + fan1State + "," + fan2State + "," + servoStatus;
 
-    sendDataToWebApp(csvData);
+    Serial.println(data);
+    sendDataToWebApp(data);
   }
 
   delay(12000);
 }
 
 void sendDataToWebApp(String data) {
-  HTTPClient http;
-
-  if (WiFi.status() == WL_CONNECTED) {
-    http.begin(webAppUrl);
-    http.addHeader("Content-Type", "application/x-www-form-urlencoded");
-    int httpResponseCode = http.POST(data);
-
-    Serial.print("HTTP Response Code: ");
-    Serial.println(httpResponseCode);
-
-    if (httpResponseCode != 200) {
-      Serial.println("Failed to send data: " + http.getString());
-    }
-    http.end();
-  } else {
-    Serial.println("WiFi not connected");
+  if (WiFi.status() != WL_CONNECTED) {
+    Serial.println("WiFi not connected. Skipping data send.");
+    return;
   }
+
+  HTTPClient http;
+  http.begin(webAppUrl);
+  http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+
+  String requestBody = "data=" + data;
+  int httpResponse = http.POST(requestBody);
+
+  if (httpResponse == 200) {
+    Serial.println("Data sent successfully!");
+  } else {
+    Serial.print("Failed to send data. Code: ");
+    Serial.println(httpResponse);
+  }
+
+  http.end();
 }
